081986183c8c30b4f98b60221c7653f1078f3bee
ParsaBordbar <parsab71@gmail.com>
2025-11-02 18:27:42 +0330

    feat(utils): slelct_a_random_phenotype

--- utils.py
+++ utils.py
@@ -6,6 +6,10 @@ def swap(item1, item2):
     item1 = item1 ^ item2
     return item1, item2
 
-def select_a_random():
+def select_a_random_chromosome():
     index = random.randint(0, 7)
+    return index
+
+def slelct_a_random_phenotype(population_size):
+    index = random.randint(0, population_size)
     return index
