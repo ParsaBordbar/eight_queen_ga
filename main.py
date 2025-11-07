d74d565aa24de27275efa527bc7749f3552836e9
ParsaBordbar <parsab71@gmail.com>
2025-11-07 19:43:19 +0330

    feat(main): new ga algorithem implementation - add scalablbiliy - add probability support - add new crossover functions - refactor survival_selection - refactor parent_selection

--- main.py
+++ main.py
@@ -1,165 +1,157 @@
-from pprint import pprint
-from utils import ga_summary, generate_chromosome, select_a_random_chromosome, slelct_a_random_phenotype, swap
+import random
+from configs import CONFIG as cfg
+from utils import ga_summary, generate_chromosome, select_a_random_chromosome, select_a_random_phenotype
 
-POPULATION_SIZE = 100
-PARENET_SELECTION_COUNT = 5
-GA_PIPLINE_ROUNDS = 50
 
-def generate_population(size):
-    population = []
-    for _ in range(size):
-        population.append(generate_chromosome())
-    return population
+def generate_population(size, N=cfg.n_queens):
+    return [generate_chromosome(N) for _ in range(size)]
 
 def fitness_evaluation(queens):
     penalty = 0
     n = len(queens)
-    
     for i in range(n):
         for j in range(i + 1, n):
             if queens[i] == queens[j]:
                 penalty += 1
             elif abs(queens[i] - queens[j]) == abs(i - j):
                 penalty += 1
-                
-    # Fitness is inverse of penalty, added 1 to avoid division by zero
-    fitness = (1 / (1 + penalty))
-    return fitness.__round__(2)
+    fitness = 1 / (1 + penalty)
+    return round(fitness, 3)
 
 
-def parent_selection(selection_count, population=generate_population(POPULATION_SIZE)):
-    sample = []
-    parents = []
-
-    # Pick 5 random parents
-    for _ in range(selection_count):
-        random_index = slelct_a_random_phenotype(population_size=len(population)-1)
-        sample.append(population[random_index])
-
-    # Evaluatting Fitnesses also we sort them here! first two index will be the best parents
-    for i in range(len(sample)):
-        fitness = fitness_evaluation(sample[i])
-        parents.append({"fitness": fitness, "chromosome": sample[i]})
-
+def parent_selection(selection_count, population):
+    sample = [population[select_a_random_phenotype(len(population) - 1)]
+              for _ in range(selection_count)]
+    parents = [{"fitness": fitness_evaluation(s), "chromosome": s} for s in sample]
+    parents.sort(key=lambda x: x['fitness'], reverse=True)
     return parents[0], parents[1]
 
 
-def crossover(parent1, parent2):
-    if (len(parent1) == 8 or parent2 == 8):
-        crossover_point = select_a_random_chromosome()
-        if crossover_point < 1: crossover_point =+ 3
+def crossover(parent1, parent2, prob=cfg.crossover_probability, mode="cutfill", cuts=1):
+    if random.random() > prob:
+        return [parent1[:], parent2[:]]
 
-        print(f"\nCrossover point at index: {crossover_point} \n")
-        
-        # Sliccing Chromosome arrays
-        parent1_firstpart = parent1[0:crossover_point]
-        parent1_secondpart = parent1[(crossover_point):]
+    N = len(parent1)
 
-        parent2_firstpart = parent2[0:crossover_point]
-        parent2_secondpart = parent2[(crossover_point):]
-        
-        parent2_cycle = parent2_secondpart + parent2_firstpart
-        parent1_cycle = parent1_secondpart + parent1_firstpart
+    # === CUT-AND-FILL (default) ===
+    if mode == "cutfill":
+        crossover_point = select_a_random_chromosome(N)
+        if crossover_point < 1:
+            crossover_point = 3
 
-        child1_secondpart = []
-        for gene in parent2_cycle:
-            if gene not in parent1_firstpart:
-                child1_secondpart.append(gene)
-                if len(parent1_secondpart) == len(child1_secondpart):
-                    break
-                continue
+        p1_first = parent1[:crossover_point]
+        p2_cycle = parent2[crossover_point:] + parent2[:crossover_point]
+        child1_tail = [g for g in p2_cycle if g not in p1_first][: N - crossover_point]
+        child1 = p1_first + child1_tail
 
-        print("this is the child1 second part")
-        pprint(child1_secondpart)
-        child1 = parent1_firstpart + child1_secondpart
-        print("\n Child 1", child1, len(child1))
-
-        child2_secondpart = []
-        for gene in parent1_cycle:
-            if gene not in parent2_firstpart:
-                child2_secondpart.append(gene)
-                if len(parent2_secondpart) == len(child2_secondpart):
-                    break
-                continue
-
-        print("\n this is the child2 second part")
-        pprint(child1_secondpart)
-        child2 = parent2_firstpart + child2_secondpart
-        print("\n Child 2", child2, len(child2))
-
+        p2_first = parent2[:crossover_point]
+        p1_cycle = parent1[crossover_point:] + parent1[:crossover_point]
+        child2_tail = [g for g in p1_cycle if g not in p2_first][: N - crossover_point]
+        child2 = p2_first + child2_tail
         return [child1, child2]
-    else:
-        raise ValueError("Chromosome length must be 8")
 
-def mution(queen):
-    mut_index_1 = select_a_random_chromosome()
-    mut_index_2 = select_a_random_chromosome()
+    #PMX CROSSOVER
+    if mode == "pmx":
+        c1, c2 = sorted(random.sample(range(N), 2))
+        child1, child2 = parent1[:], parent2[:]
 
-    mut_el1, mut_el2 = swap(queen[mut_index_1], queen[mut_index_2])
+        child1[c1:c2], child2[c1:c2] = parent2[c1:c2], parent1[c1:c2]
 
-    queen[mut_index_1] = mut_el1
-    queen[mut_index_2] = mut_el2
-    return queen
+        # mapping
+        mapping1 = {parent2[i]: parent1[i] for i in range(c1, c2)}
+        mapping2 = {parent1[i]: parent2[i] for i in range(c1, c2)}
 
-def survival_selection(population, children ,population_size=POPULATION_SIZE):
-    population_fitnesses = []
+        # fix duplicates
+        def map_gene(gene, mapping):
+            while gene in mapping:
+                gene = mapping[gene]
+            return gene
 
-    # Population's fittness evaluation
-    for sample in population:
-        fitness = fitness_evaluation(sample)
-        population_fitnesses.append({"fitness": fitness, "chromosome": sample})
-    population_fitnesses.sort(key=lambda x: x['fitness'], reverse=True)
+        for i in list(range(0, c1)) + list(range(c2, N)):
+            child1[i] = map_gene(child1[i], mapping1)
+            child2[i] = map_gene(child2[i], mapping2)
+        return [child1, child2]
 
-    # Children's fittness evaluation
-    children_fitnesses = []
-    for child in children:
-        child_fitness = fitness_evaluation(child)
-        children_fitnesses.append({"fitness": child_fitness, "chromosome": child})
-    print("\n Children's fitnesses: ")
-    pprint(children_fitnesses)
+    #MULTI-CUT (2 or 3 cuts)
+    if mode == "multi":
+        cuts = min(cuts, 3)
+        cut_points = sorted(random.sample(range(1, N - 1), cuts))
+        parts1, parts2 = [], []
+        last = 0
+        for cp in cut_points + [N]:
+            parts1.append(parent1[last:cp])
+            parts2.append(parent2[last:cp])
+            last = cp
+        child1, child2 = [], []
+        for i in range(len(parts1)):
+            if i % 2 == 0:
+                child1 += parts1[i]
+                child2 += parts2[i]
+            else:
+                child1 += parts2[i]
+                child2 += parts1[i]
+        return [child1, child2]
+    return [parent1[:], parent2[:]]
 
-    # The Actual Survival Selection
-    for i, sample in enumerate(population_fitnesses):
-        for j, child in enumerate(children_fitnesses):    
-            if sample['fitness'] < children_fitnesses[j]['fitness']:
-                population_fitnesses.pop(i)
-                population_fitnesses.insert(i, children_fitnesses[0])
-                children_fitnesses.pop(j)
-                population_fitnesses.sort(key=lambda x: x['fitness'], reverse=True)
-    
-    return population_fitnesses[:population_size]
+
+def mutation(chromosome, prob=cfg.mutation_probability, mode=cfg.mutation_type):
+    if random.random() > prob:
+        return chromosome
+
+    N = len(chromosome)
+    i, j = select_a_random_chromosome(N), select_a_random_chromosome(N)
+    if mode == "swap":
+        chromosome[i], chromosome[j] = chromosome[j], chromosome[i]
+    elif mode == "bitwise":
+        chromosome[i] = random.choice([x for x in range(N) if x not in chromosome or x == chromosome[i]])
+    return chromosome
+
+
+def survival_selection(population, children, population_size=cfg.population_size):
+    all_samples = population + children
+    fitnesses = [{"fitness": fitness_evaluation(s), "chromosome": s} for s in all_samples]
+    fitnesses.sort(key=lambda x: x['fitness'], reverse=True)
+    return [f["chromosome"] for f in fitnesses[:population_size]]
+
 
 def fitness_mean(population):
-    total_fitness = 0
-    for sample in population:
-        total_fitness += fitness_evaluation(sample)
-    mean_fitness = total_fitness / len(population)
-    return mean_fitness.__round__(4)
+    return round(sum(fitness_evaluation(s) for s in population) / len(population), 4)
 
 
-def simple_GA_pipline(rounds=GA_PIPLINE_ROUNDS ,population_size=POPULATION_SIZE, parent_selection_count=PARENET_SELECTION_COUNT):
-    population = generate_population(population_size)
+def simple_GA_pipeline(rounds=cfg.ga_pipeline_rounds,
+                       population_size=cfg.population_size,
+                       parent_selection_count=cfg.parent_selection_count,
+                       N=cfg.n_queens):
+    population = generate_population(population_size, N)
+    evaluations = 0
 
-    for _ in range(rounds):
+    for gen in range(rounds):
         parents = parent_selection(parent_selection_count, population)
-        crossover_result = crossover(parents[0]['chromosome'], parents[1]['chromosome'])
-        mut1 = mution(crossover_result[0])
-        mut2 = mution(crossover_result[1])
-        children = [mut1, mut2]
-        population_fitnesses = survival_selection(population, children, population_size)
+        evaluations += len(parents)
 
-        population = []
-        for sample in population_fitnesses:
-            population.append(sample['chromosome'])
+        crossover_result = crossover(parents[0]['chromosome'], parents[1]['chromosome'])
+        evaluations += 2
+
+        children = [mutation(c, cfg.mutation_probability, cfg.mutation_type) for c in crossover_result]
+        evaluations += len(children)
+
+        population = survival_selection(population, children, population_size)
         mean_fitness = fitness_mean(population)
-        
-        ga_summary(population, parents, crossover_result, children, population, population_fitnesses, mean_fitness)
+
+        ga_summary(population, parents, crossover_result, children, population, population, mean_fitness, evaluations)
+
+        if any(fitness_evaluation(p) == 1 for p in population):
+            print(f"✅ Solution found at generation {gen} after {evaluations} evaluations!")
+            break
+        if evaluations >= cfg.max_evaluations:
+            print("⚠️ Terminated after reaching evaluation limit.")
+            break
 
     return population
 
 
 def main():
-   simple_GA_pipline(10, POPULATION_SIZE, PARENET_SELECTION_COUNT)
+    simple_GA_pipeline(rounds=500, population_size=cfg.population_size, parent_selection_count=cfg.parent_selection_count, N=cfg.n_queens)
 
 if __name__ == "__main__":
     main()
