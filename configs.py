3da96b36bfb1afca3016d9730b32796d85d4bbbb
ParsaBordbar <parsab71@gmail.com>
2025-11-14 12:44:03 +0330

    feat(configs): config module

--- configs.py
+++ configs.py
@@ -1,20 +1,12 @@
-import os
-from dotenv import load_dotenv
-from dataclasses import dataclass
-
-load_dotenv()
-
-
-@dataclass(frozen=True)
 class Config:
-    population_size: int = int(os.getenv("POPULATION_SIZE", 100))
-    parent_selection_count: int = int(os.getenv("PARENT_SELECTION_COUNT", 5))
-    ga_pipeline_rounds: int = int(os.getenv("GA_PIPELINE_ROUNDS", 1000))
-    max_evaluations: int = int(os.getenv("MAX_EVALUATIONS", 10000))
-    n_queens: int = int(os.getenv("N_QUEENS", 8))
-    mutation_probability: float = float(os.getenv("MUTATION_PROBABILITY", 0.5))
-    crossover_probability: float = float(os.getenv("CROSSOVER_PROBABILITY", 1.0))
-    mutation_type: str = os.getenv("MUTATION_TYPE", "swap")
+    population_size: int = 100
+    parent_selection_count: int = 5
+    ga_pipeline_rounds: int = 1000          # rounds of running of the algorithm
+    max_evaluations: int = 10000
+    n_queens: int = 8
+    mutation_probability: float = 0.5       # values from the HW: 0.2, 0.5 & 1
+    crossover_probability: float = 1.0      # values from the HW: 0.5 & 1 
+    mutation_type: str = "bitwise"             # values from the HW: swap & bitwise
 
 
-CONFIG = Config()
+cfg = Config()
